a5aed470b40efeb0361ed1fe1d9db143942d822e
sallman <kostianaddur@gmail.com>
2023-05-06 11:58:08 +0300

    main file

---
+++ main.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+red="\e[31m"
+none="\e[0m"
+
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+read -p "${bold}Enter the folder where the iso is located (The folder must be in $HOME)${normal}: " fullname
+echo -e "${bold}Folder: ${red}/home/$USER/${fullname}${none}${normal}"
+
+files=(~/${fullname}/*.iso)
+
+# Prompt the user to select a file
+echo "Select a file:"
+select file in "${files[@]}"; do
+    # Check if a valid option was selected
+    if [[ -n $file ]]; then
+        echo "You selected: $file"
+        break
+    else
+        echo "Invalid option. Try again."
+    fi
+done
+
+echo ""
+
+sleep 0.5
+
+lsblk
+function usb() {
+    echo ""
+    read -p "Enter your USB drive name: " usbdrive
+    echo ""
+    read -e -p "Your flash drive: /dev/${usbdrive} " choice
+}
+usb
+
+# Use a while loop to repeat the prompt until the user selects "yes"
+while [ "$choice" != "yes" ]; do
+    if [ "$choice" = "no" ]; then
+        echo "you select no"
+        sleep 1
+        usb
+    fi
+done
+
+sudo dd if=${file} of=/dev/${usbdrive} bs=4M status=progress && sync
