ad648ac48a4996ead8ad529b4cb27e611d0ec514
rendick <rendick123@proton.me>
2024-01-05 13:38:20 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -2,31 +2,31 @@
 
 copy () {
 	chmod +x isowrt.sh
-	if [[ $1 == "system" ]] ; then
-		sudo cp isowrt.sh /usr/bin/local
-		echo 'Note that you need to add /usr/bin/local to your $PATH in your shell config file (e.g. .bashrc or .zshrc. Usually it already added, but you have to check it!'
-	elif [[ $1 == "user" ]] ; then
-		cp isowrt.sh ~/.local/bin/
-		echo 'Note that you need to add $HOME/.local/bin to your $PATH in your shell config file (e.g. .bashrc or .zshrc. Usually it already added, but you have to check it!'
-	else
+	if [[ $1 == "yes" ]] ; then
+		cp isowrt.sh isowrt
+		sudo chmod +x isowrt
+		sudo mv isowrt /bin/
+	elif [[ $1 == "no" ]] ; then
+		exit
+		else
 		echo 'invalid installation type'
 	fi
 }
 
 PS3='Installation type: '
-types=("system" "user")
+types=("yes" "no")
 
 echo
 
 select type in "${types[@]}" ; do
-	if [[ -n $type ]] ; then
-		echo 'You selected: $type'
+	if [[ -n $types ]] ; then
+		echo 'You selected: $1'
 		break
 	else
 		echo 'Please select between user and system installation'
 	fi
 done
 
-echo ''
+sleep 2
 
 copy $type
