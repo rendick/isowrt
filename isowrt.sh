2a75d9bc23bfca21d2a959627c0d0373a9d1436e
Richard Benson <91757099+rendick@users.noreply.github.com>
2024-01-13 12:46:21 +0200

    Update isowrt.sh

--- isowrt.sh
+++ isowrt.sh
@@ -33,7 +33,7 @@ function usb() {
     echo ""
     read -p "Enter your USB drive name: " usbdrive
     echo ""
-    read -e -p "Your flash drive: /dev/${usbdrive}/ (y or n) " choice
+    read -e -p "Your flash drive: /dev/${usbdrive} (y or n) " choice
 }
 
 usb
@@ -48,4 +48,5 @@ done
 
 echo "Wait..."
 
-sudo dd if="${file}" of="/dev/${usbdrive}" bs=4M status=progress && sync
+# sudo dd if="${file}" of="/dev/${usbdrive}" bs=4M && sync
+pv -ptearb ${file} | dd iflag=fullblock of=/dev/${usbdrive} bs=4M
